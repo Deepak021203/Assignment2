272bc5eb6a8aa9f95bb4e955b35539b270f89f0e
Deepak021203 <124549040+Deepak021203@users.noreply.github.com>
2023-02-07 22:00:35 +0530

    Initial commit

---
+++ src/stringtypes/java/string13.java
@@ -0,0 +1,11 @@
+package stringtypes.java;
+
+public class string13 {
+    public static void main(String args[]){
+        String s1="";
+        String s2="javatpoint";
+
+        System.out.println(s1.isEmpty());
+        System.out.println(s2.isEmpty());
+    }
+}
